ee52b85536bd6ba3c59ee36db04e96128fa67859
Nilkanta Bhattacharjee <cogjava405@iiht.tech>
2022-06-17 19:45:15 +0530

    Table structure ans CSS

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,18 +10,21 @@ import { AssociateDetails } from './model/associate-details';
 })
 export class AppComponent implements OnInit {
   searchForm: FormGroup = this.fb.group({
-    name: [{value: '', disabled: false}],
-    associateId: [{value: '', disabled: false}],
-    skill: [{value: '', disabled: false}]
+    name: [{ value: '', disabled: false }],
+    associateId: [{ value: '', disabled: false }],
+    skill: [{ value: '', disabled: false }]
   });
   associateDetailList: AssociateDetails[] = [];
   associateDetail: any;
+  showMsg: boolean = false;
   constructor(
     private fb: FormBuilder,
     private appService: AppService
   ) { }
 
-  ngOnInit() { }
+  ngOnInit() {
+    //this.searchForm.controls['name'].disable();
+  }
 
   searchAssociates() {
     console.log('Search associate called..' + this.searchForm.value['associateId']);
@@ -30,47 +33,43 @@ export class AppComponent implements OnInit {
     let searchValue: string;
     searhType = 'name';
     searchValue = this.searchForm.value['name'];
-    if(searchValue == '') {
+    if (searchValue == '') {
       searhType = 'associateId';
       searchValue = this.searchForm.value['associateId'];
     }
-    if(searchValue == '') {
+    if (searchValue == '') {
       searhType = 'skill';
       searchValue = this.searchForm.value['skill'];
     }
-    this.appService.getAsscociateDetails(searhType, searchValue).subscribe({
-      next: (successResponse: any) => {
-        console.log('Success response : ' + successResponse);
-        if (successResponse.profiles) {
-          //this.associateDetailList = (successResponse.profiles as Partial<AssociateDetails>[]) ?? [];
-          console.log('successResponse.profiles: ' + successResponse.profiles[0].userId);
-          this.associateDetail = successResponse.profiles[0];
-          let profiles: any[] = successResponse.profiles;
-          profiles.forEach(val => this.associateDetailList.push(Object.assign({}, val)));
-          console.log('associateDetailList: ' + this.associateDetailList[0].name);
-          console.log('associateDetailList: ' + this.associateDetailList[0].skills[0].name);
+    if (searchValue == '') {
+      this.showMsg = true;
+    } else {
+      this.appService.getAsscociateDetails(searhType, searchValue).subscribe({
+        next: (successResponse: any) => {
+          console.log('Success response : ' + successResponse);
+          if (successResponse.profiles) {
+            //this.associateDetailList = (successResponse.profiles as Partial<AssociateDetails>[]) ?? [];
+            console.log('successResponse.profiles: ' + successResponse.profiles[0].userId);
+            this.associateDetail = successResponse.profiles[0];
+            let profiles: any[] = successResponse.profiles;
+            profiles.forEach(val => this.associateDetailList.push(Object.assign({}, val)));
+            console.log('associateDetailList: ' + this.associateDetailList[0].name);
+            console.log('associateDetailList: ' + this.associateDetailList[0].skills[0].name);
+          }
+        },
+        error: (error) => {
+          console.log('error occured : ' + error);
+        },
+        complete: () => {
+
         }
-      },
-      error: (error) => {
-        console.log('error occured : ' + error);
-      },
-      complete: () => {
-
-      }
-    });
-  }
-
-  initFormModel(input?: any){
-    const formModel = {
-      name: [{value: '', disabled: false}],
-      associateId: [{value: '', disabled: false}],
-      skill: [{value: '', disabled: false}],
-    };
-    return formModel;
+      });
+    }
   }
 
   reset(form: FormGroup) {
     this.associateDetailList.length = 0;
+    this.showMsg = false;
     Object.keys(form.controls).forEach(field => {
       const control = form.get(field);
       control?.setValue('');
